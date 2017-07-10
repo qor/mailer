1d207505bfeffe4fc9f499a9a5b4232c3b5e790e
Jinzhu <wosmvp@gmail.com>
2017-07-10 16:45:48 +0800

    Add merge method

--- email.go
+++ email.go
@@ -24,6 +24,46 @@ type Attachment struct {
 	Content  io.Reader
 }
 
-func (Email) Merge(Email) Email {
-	return Email{}
+// Merge merge email struct and create a new one
+func (email Email) Merge(e Email) Email {
+	if len(e.TO) > 0 {
+		email.TO = e.TO
+	}
+
+	if len(e.CC) > 0 {
+		email.CC = e.CC
+	}
+
+	if len(e.BCC) > 0 {
+		email.BCC = e.BCC
+	}
+
+	if e.From == nil {
+		email.From = e.From
+	}
+
+	if e.ReplyTo == nil {
+		email.ReplyTo = e.ReplyTo
+	}
+
+	if e.Subject != "" {
+		email.Subject = e.Subject
+	}
+	if e.Headers != nil {
+		email.Headers = e.Headers
+	}
+
+	if len(e.Attachments) > 0 {
+		email.Attachments = e.Attachments
+	}
+
+	if e.Template != "" {
+		email.Template = e.Template
+	}
+
+	if e.Layout != "" {
+		email.Layout = e.Layout
+	}
+
+	return email
 }
