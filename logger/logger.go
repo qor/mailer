a6cd53d6557eb25c839291b5846b38ec1fad3a44
Henry <cryptix@riseup.net>
2017-11-12 17:21:38 +0100

    idiomatic use of result buffer

--- logger/logger.go
+++ logger/logger.go
@@ -28,19 +28,19 @@ func New(config *Config) *Sender {
 
 // Send send email with GoMail
 func (sender *Sender) Send(email mailer.Email) error {
-	var result bytes.Buffer
+	var result = new(bytes.Buffer)
 
 	formatAddress := func(key string, addresses []mail.Address) {
 		var emails []string
 
 		if len(addresses) > 0 {
-			result.WriteString(fmt.Sprintf("%v: ", key))
+			fmt.Fprintf(result, "%v: ", key)
 
 			for _, address := range addresses {
 				emails = append(emails, address.String())
 			}
 
-			result.WriteString(strings.Join(emails, ", ") + "\n")
+			fmt.Fprintf(result, "%s\n", strings.Join(emails, ", "))
 		}
 	}
 
@@ -57,29 +57,29 @@ func (sender *Sender) Send(email mailer.Email) error {
 	}
 
 	if email.Subject != "" {
-		result.WriteString(fmt.Sprintf("Subject: %v\n", email.Subject))
+		fmt.Fprintf(result, "Subject: %v\n", email.Subject)
 	}
 
 	if email.Headers != nil {
 		for key, value := range email.Headers {
-			result.WriteString(fmt.Sprintf("%v: %v\n", key, value))
+			fmt.Fprintf(result, "%v: %v\n", key, value)
 		}
 	}
 
 	for _, attachment := range email.Attachments {
 		if attachment.Inline {
-			result.WriteString(fmt.Sprintf("\nContent-Disposition: inline; filename=\"%v\"\n\n", attachment.FileName))
+			fmt.Fprintf(result, "\nContent-Disposition: inline; filename=\"%v\"\n\n", attachment.FileName)
 		} else {
-			result.WriteString(fmt.Sprintf("\nContent-Disposition: attachment; filename=\"%v\"\n\n", attachment.FileName))
+			fmt.Fprintf(result, "\nContent-Disposition: attachment; filename=\"%v\"\n\n", attachment.FileName)
 		}
 	}
 
 	if email.Text != "" {
-		result.WriteString(fmt.Sprintf("\nContent-Type: text/plain; charset=UTF-8\n%v\n", email.Text))
+		fmt.Fprintf(result, "\nContent-Type: text/plain; charset=UTF-8\n%v\n", email.Text)
 	}
 
 	if email.HTML != "" {
-		result.WriteString(fmt.Sprintf("\nContent-Type: text/html; charset=UTF-8\n%v\n", email.HTML))
+		fmt.Fprintf(result, "\nContent-Type: text/html; charset=UTF-8\n%v\n", email.HTML)
 	}
 
 	fmt.Println(result.String())
